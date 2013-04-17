0714d21b51d938be85233b3fbb4431c8b400807d
Marius Gundersen <gundersen@gmail.com>
2013-04-17 22:52:15 +0200

    made the jsTestDriver work

--- Specs/requireConfig.js
+++ Specs/requireConfig.js
@@ -1,7 +1,7 @@
-require.config({
-    baseUrl: "/test/Specs",
-    paths: {
-		"ordnung": "../Source/ordnung",
-		"knockout": "../Source/Libs/knockout-2.1.0"
-    }
-});
+require.config(moquire.config({
+	baseUrl: "/test/Specs",
+	paths: {
+		"ordnung": "/test/Source/ordnung",
+		"knockout": "/test/Source/Libs/knockout-2.1.0"
+	}
+}));

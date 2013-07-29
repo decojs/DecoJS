46a5527e452c5cae9adec68b8f535611c62fc2df
Marius Gundersen <gundersen@gmail.com>
2013-07-29 15:38:40 +0200

    made the test jazzy

--- Specs/Modules/for_load/when_loading_viewmodels.js
+++ Specs/Modules/for_load/when_loading_viewmodels.js
@@ -1,42 +1,38 @@
-moquire(["ordnung/load"], function(load){
+describe("when loading viewmodels", ["ordnung/load"], function(load){
 
 
 	function functionName(m){
 		return m.name || m.toString().match(/function\s+([^(]+)/)[1];
 	}
 
-	describe("when loading viewmodels", function(){
+	var dummyVM;
 
-		var dummyVM;
+	beforeEach(function(done){
 
-		new Async(this).beforeEach(function(done){
+		var elm = document.createElement("div");
+		var div = document.createElement("div");
+		div.setAttribute("data-viewmodel", "dummyVM");
+		elm.appendChild(div);
 
-			var elm = document.createElement("div");
-			var div = document.createElement("div");
-			div.setAttribute("data-viewmodel", "dummyVM");
-			elm.appendChild(div);
+		dummyVM = sinon.spy();
 
-			dummyVM = sinon.spy();
-
-			define("dummyVM", [], function(){
-				return function DummyVM(){
-					dummyVM(this);
-					done();
-				};
-			});
-
-			because: {
-				load(elm);
-			}
+		define("dummyVM", [], function(){
+			return function DummyVM(){
+				dummyVM(this);
+				done();
+			};
 		});
 
-		it("should find all the viewmodels in the dom tree", function(){
-			expect(dummyVM.callCount).toBe(1);
-		});
-
-		it("should call the viewmodule as a constructor", function(){
-			expect(functionName(dummyVM.getCall(0).args[0].constructor)).toBe("DummyVM");
-		});
+		because: {
+			load(elm);
+		}
 	});
 
+	it("should find all the viewmodels in the dom tree", function(){
+		expect(dummyVM.callCount).toBe(1);
+	});
+
+	it("should call the viewmodule as a constructor", function(){
+		expect(functionName(dummyVM.getCall(0).args[0].constructor)).toBe("DummyVM");
+	});
 });

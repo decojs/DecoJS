72465c21db18d71c1e3f8f02920ddd0e832ce634
Marius Gundersen <gundersen@gmail.com>
2013-07-30 14:33:01 +0200

    Started working on the functionality for manipulating the page outlet

---
+++ Source/ordnung/spa/Outlet.js
@@ -0,0 +1,42 @@
+define([
+	"knockout"
+], function(
+	ko
+){
+
+	function Outlet(element){
+		this.element = element;
+	}
+
+	Outlet.prototype.unloadCurrentPage = function(){
+
+	};
+
+	Outlet.prototype.setPageContent = function(content){
+
+	};
+
+	Outlet.prototype.getPageTitle = function(){
+
+	};
+
+	Outlet.prototype.setDocumentTitle = function(title){
+
+	};
+
+	Outlet.prototype.extractAndRunPageJavaScript = function(){
+
+	};
+
+	Outlet.prototype.indicatePageIsLoading = function(){
+
+	};
+
+	Outlet.prototype.pageHasLoaded = function(){
+
+	};
+
+
+	return Outlet;
+
+});
---
+++ Specs/Modules/for_spa/for_Outlet/when_creating_an_outlet.js
@@ -0,0 +1,54 @@
+describe("when creating an outlet", [
+	"ordnung/spa/Outlet"
+], function(
+	Outlet
+){
+
+	var outlet;
+
+	beforeEach(function(){
+		var elm = document.createElement("div");
+
+		outlet = new Outlet(elm);
+	});
+	
+	it("should take one argument", function(){
+		expect(Outlet.length).toBe(1);
+	});
+
+	it("should have a way to unload the current page", function(){
+		expect(outlet.unloadCurrentPage).toBeA(Function);
+		expect(outlet.unloadCurrentPage.length).toBe(0);
+	});
+
+	it("should have a way to set the page content", function(){
+		expect(outlet.setPageContent).toBeA(Function);
+		expect(outlet.setPageContent.length).toBe(1);
+	});
+
+	it("should have a way to get the current page title", function(){
+		expect(outlet.getPageTitle).toBeA(Function);
+		expect(outlet.getPageTitle.length).toBe(0);
+	});
+
+	it("should have a way to set the document title", function(){
+		expect(outlet.setDocumentTitle).toBeA(Function);
+		expect(outlet.setDocumentTitle.length).toBe(1);
+	});
+
+	it("should have a way to extract and run the page JavaScript", function(){
+		expect(outlet.extractAndRunPageJavaScript).toBeA(Function);
+		expect(outlet.extractAndRunPageJavaScript.length).toBe(0);
+	});
+
+	it("should have a way to indicate that a page is loading", function(){
+		expect(outlet.indicatePageIsLoading).toBeA(Function);
+		expect(outlet.indicatePageIsLoading.length).toBe(0);
+	});
+
+	it("should be told when the page is done loading", function(){
+		expect(outlet.pageHasLoaded).toBeA(Function);
+		expect(outlet.pageHasLoaded.length).toBe(0);
+	});
+
+});

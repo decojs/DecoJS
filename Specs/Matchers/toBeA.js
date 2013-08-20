fce0080a0cfb73fc2012d6d811aa6fd19bcd159d
Marius Gundersen <gundersen@gmail.com>
2013-08-20 17:19:54 +0200

    moved jazzmine (and its dependencies) out of the project and loaded it via npm instead

---
+++ Specs/Matchers/toBeA.js
@@ -0,0 +1,42 @@
+
+
+
+(function(){
+  
+  function functionName(m){
+    return m.name || m.toString().match(/function\s+([^(]+)/)[1];
+  }
+
+  function typeOf(obj) {
+    var typeString = ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1];
+    if(typeString == "Object"){
+      return functionName(obj.constructor);
+    }else{
+      return typeString;
+    }
+  }
+
+  function toBeA(aOrAn, type){
+    var actual = typeOf(this.actual);
+    var notText = this.isNot ? " not" : "";
+    var expected = functionName(type);
+
+    this.message = function(){
+      return "expected " + actual + notText + " to be " + aOrAn + expected;
+    }
+
+    return actual === expected;
+  }
+
+  beforeEach(function(){
+    this.addMatchers({
+      toBeA: function(type){
+        return toBeA.call(this, "a ", type)
+      },
+      toBeAn: function(type){
+        return toBeA.call(this, "an ", type)
+      }
+    });
+  });
+
+})();

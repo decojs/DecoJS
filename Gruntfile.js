65ab200ebda9e97f06b124ed814aa795f73fce24
Marius Gundersen <Marius.Gundersen@ciber.com>
2013-09-22 18:51:59 +0200

    Automating the process of releasing a new version

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,8 +62,22 @@ module.exports = function(grunt) {
       continuous: {
         configFile: 'karma.conf.js',
         singleRun: true,
-        browsers: ['Firefox', 'IE']
+        browsers: ['PhantomJS']
       },
+    },
+    bumpup: {
+      //https://github.com/Darsain/grunt-bumpup
+      options: {
+          updateProps: {
+              pkg: 'package.json'
+          }
+      },
+      files: ['package.json', 'bower.json']
+    },
+    tagrelease: {
+      //https://github.com/Darsain/grunt-tagrelease
+      file: 'package.json',
+      commit: true
     }
   });
 
@@ -71,11 +85,22 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-bumpup');
+  grunt.loadNpmTasks('grunt-tagrelease');
 
   // Default task(s).
   grunt.registerTask('install', ['bower']);
   grunt.registerTask('build', ['requirejs', 'uglify']);
   grunt.registerTask('test', ['karma:unit']);
   grunt.registerTask('default', ['bower', 'requirejs', 'uglify', 'karma:continuous']);
+  grunt.registerTask('release', function (type) {
+    //grunt release:major|minor|patch
+    type = type ? type : 'patch';
+    grunt.task.run('karma:continuous');
+    grunt.task.run('requirejs');
+    grunt.task.run('uglify');
+    grunt.task.run('bumpup:' + type);
+    grunt.task.run('tagrelease');
+  });
 
 };

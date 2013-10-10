4f5220d5f68d2268ecea8693a4e202b988588ec1
Marius Gundersen <me@mariusgundersen.net>
2013-10-10 21:43:40 +0200

    Added ability to subscribe to subscriptions and unsubscriptions.
    A publisher can now be notified when someone subscribes to an event. No information about the subscriber is sent to the subscribe-subscriber.

--- Specs/Modules/for_proclaimWhen/when_unsubscribing_from_an_event.js
+++ Specs/Modules/for_proclaimWhen/when_unsubscribing_from_an_event.js
@@ -1,4 +1,4 @@
-describe("when unsubscribing to an event", ["ordnung/proclaimWhen"], function(proclaimWhen){
+describe("when unsubscribing from an event", ["ordnung/proclaimWhen"], function(proclaimWhen){
 
 	
 	var when,

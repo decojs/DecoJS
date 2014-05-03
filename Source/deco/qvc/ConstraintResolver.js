3eabf4484ad3d4a34b626c1ebddf7b36bd2c0c43
Marius Gundersen <me@mariusgundersen.net>
2014-05-03 18:19:07 +0200

    This fixes issue #14, where executables were held on to for too long. Since an executable has event listeners, which are usually created inside viewmodels, the executable held on to the entire viewmodel through the closure of the event listener.

--- Source/deco/qvc/ConstraintResolver.js
+++ Source/deco/qvc/ConstraintResolver.js
@@ -1,5 +1,7 @@
 define([], function(){
 
+  var STATE_LOADING = 'loading';
+  var STATE_LOADED = 'loaded';
 
   function findConstraint(name, constraints) {
     for (var i = 0; i < constraints.length; i++) {
@@ -17,7 +19,8 @@ define([], function(){
         validatable.applyConstraints(fields);
       });
       constraint.fields = fields;
-      constraint.state = "loaded";
+      constraint.state = STATE_LOADED;
+      constraint.validatables = [];
     }
   }
 
@@ -29,15 +32,15 @@ define([], function(){
   
   ConstraintResolver.prototype.applyValidationConstraints = function(name, validatable){
     var constraint = findConstraint(name, this.constraints);
-    if(constraint == false){
+    if(constraint === false){
       this.constraints.push({
         name: name,
-        state: "loading",
+        state: STATE_LOADING,
         validatables: [validatable]
       });
       this.qvc.loadConstraints(name, constraintsLoaded.bind(this));
     }else{
-      if(constraint.state === "loading"){
+      if(constraint.state === STATE_LOADING){
         constraint.validatables.push(validatable);
       }else{
         validatable.applyConstraints(constraint.fields);
